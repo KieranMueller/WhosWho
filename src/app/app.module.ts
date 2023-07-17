1db36db883136774f12cb1dac7327edcff794ffd
KieranMueller <kieran98mueller@gmail.com>
2023-07-17 11:18:52 -0500

    Added routing, one test method in home component, and created game component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http'
 
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
+import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { AppRoutingModule } from './app-routing.module'
 
-const routes: Routes = [{ path: "", component: HomeComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent }]
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HomeComponent, GameComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes),
+        AppRoutingModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
