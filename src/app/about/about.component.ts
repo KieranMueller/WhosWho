ffa34ab8b0051de28d3d58fd80a2f89093a9170f
NIcoleR1234 <nicolrmn1234@gmail.com>
2023-07-20 18:09:48 -0400

    Avatars and Bios in the about section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface UserData {
+  avatar_url: string;
+  bio: string;
+}
 
 @Component({
   selector: 'app-about',
@@ -7,9 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  avatarUrl1: string = '';
+  bio1: string = '';
+  avatarUrl2: string = '';
+  bio2: string = '';
 
-  ngOnInit(): void {
-  }
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const url1 = 'https://api.github.com/users/KieranMueller';
+    const url2 = 'https://api.github.com/users/NicoleR1234';
+
+    this.fetchDataAndUpdateDOM(url1, url2);
 
 }
+async fetchDataAndUpdateDOM(url1: string, url2: string): Promise<void> {
+  try {
+    const [data1, data2] = await Promise.all([
+      this.http.get<UserData>(url1).toPromise(),
+      this.http.get<UserData>(url2).toPromise()
+    ]);
+
+    if (data1 !== undefined) {
+      this.avatarUrl1 = data1.avatar_url;
+      this.bio1 = data1.bio;
+    }
+
+    if (data2 !== undefined) {
+      this.avatarUrl2 = data2.avatar_url;
+      this.bio2 = data2.bio;
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+}
+
